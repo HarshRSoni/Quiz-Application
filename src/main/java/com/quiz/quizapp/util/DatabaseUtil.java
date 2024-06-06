346d68d704b6d9c36d4d25af81c59c3b73380a0b
Harsh R Soni <harshrajsoni03@gmail.com>
2024-06-06 15:28:29 +0530

    css media query

--- src/main/java/com/quiz/quizapp/util/DatabaseUtil.java
+++ src/main/java/com/quiz/quizapp/util/DatabaseUtil.java
@@ -5,11 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/quizdb"; // Database URL
-    private static final String USER = "root";
-    private static final String PASSWORD = "mypassword";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/quizdb"; // Database URL
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PASSWORD = "mypassword";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        Connection dbConnection = null;
+
+        try {
+            dbConnection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+            System.out.println("Connection to the database established successfully.");
+        }
+
+        catch (SQLException sqlException) {
+            System.err.println("Failed to connect to the database.");
+            sqlException.printStackTrace();
+        }
+
+        return dbConnection;
     }
 }
